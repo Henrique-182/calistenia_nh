fb24a0f535d5bb8fa5124ccfe0d48663f5060277
Henrique Augusto Lobo <henriqueaugustolobo.dev@gmail.com>
2022-01-20 08:35:47 -0300

    Changing the logic of the AuthController to look at the logged-in user

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,4 +1,5 @@
 import 'package:calistenia_nh/shared/auth/auth_controller.dart';
+import 'package:calistenia_nh/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -12,7 +13,11 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user = UserModel(
+        name: response!.displayName!,
+        photoURL: response.photoUrl,
+      );
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
